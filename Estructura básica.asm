ca2ce9e67f3ba24ab92f4a458d08fc8160cee2e4
Axel Rodriguez <alejandrom9712@gmail.com>
2019-10-10 08:05:30 -0700

    Arreglada la estructura básica

--- Estructura básica.asm
+++ Estructura básica.asm
@@ -7,10 +7,10 @@
 .CODE
 Programa:       ; Etiqueta de inicio del Programa
 ;inicializar el Programa
-MOV AX @DATA    ; Guardando dirección de inicio segmento de
+MOV AX, @DATA    ; Guardando dirección de inicio segmento de
 MOV DS, AX
 
 ; Finalizar el programa
-MOV AH, 4C  ; finaliza el proceso
+MOV AH, 4CH  ; finaliza el proceso
 INT 21H     ; Ejecuta la interrupción
 END Programa
